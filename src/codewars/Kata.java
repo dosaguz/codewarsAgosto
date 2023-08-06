256a8fc56fd52e5fd3109894892ccd4ff0ab3f4a
dosaguz <xdg2001@gmail.com>
2023-08-06 03:46:57 +0200

    06/08

---
+++ src/codewars/Kata.java
@@ -0,0 +1,26 @@
+package codewars;
+
+public class Kata {
+	  public static String reverseWords(final String original)
+	  {
+	    // Have at it
+		  String salida = "";
+		  char c;
+		  String[] palabras =original.split(" ");
+
+  if (palabras.length == 0) {
+    return original;
+  }
+		  for (String p : palabras) {
+			  for (int i=p.length()-1;i>=0;i--) {
+				 c= p.charAt(i);
+				 salida=salida+c;
+			 }
+			  salida=salida+" ";
+			 }
+		  return salida.substring(0, salida.length() - 1);	 
+	  }
+	  public static void main(String[] args) {
+		System.out.println(reverseWords("The quick brown fox jumps over the lazy dog."));
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author xdg20
+ *
+ */
+module codewars {
+}
